1b6658694d7ec47649cbc74a55a6f5add6fec9c7
violetann894 <rachelhuss2976@gmail.com>
2025-02-05 02:00:13 -0500

    Fixed answer to significant digit problem and added 3 decimal point string formatting

--- src/BirthdayProgramTester.java
+++ src/BirthdayProgramTester.java
@@ -36,9 +36,9 @@ public class BirthdayProgramTester {
 
         Number of Trials: 100000
         Class size: 33
-        Range of probabilities: 77.77% - 78.65%
-        Significant digit: Tens, ones, tenths and hundredths place
-        Number of significant digits: 4
+        Range of probabilities: 77.337% - 77.556%
+        Significant digit: Tens, ones, tenths, hundredths and thousandths place
+        Number of significant digits: 5
 
         Conclusion: As the trials increase, the accuracy of the probabilities and the number of significant digits
         increase.
@@ -51,8 +51,8 @@ public class BirthdayProgramTester {
         int classSize = 33;
 
         //Print out a formatted string of the probability that two people share a birthday in the same class
-        System.out.printf("The probability that two people share a birthday in a %d person sized class: %.2f", classSize,
-                tester.runTrials(10000, classSize)*100);
+        System.out.printf("The probability that two people share a birthday in a %d person sized class: %.3f", classSize,
+                tester.runTrials(100000, classSize)*100);
 
 
     }
