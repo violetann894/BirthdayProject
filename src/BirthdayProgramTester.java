817387c4c676e413cd26e4648ac51e3464727e75
violetann894 <rachelhuss2976@gmail.com>
2025-02-04 23:50:15 -0500

    Changed the classSize to reflect the amount of people in our class

--- src/BirthdayProgramTester.java
+++ src/BirthdayProgramTester.java
@@ -10,10 +10,12 @@ public class BirthdayProgramTester {
         BirthdayProgram tester = new BirthdayProgram();
 
         //Initialize the classSize
-        int classSize = 100;
+        int classSize = 33;
 
         //Print out a formatted string of the probability that two people share a birthday in the same class
-        System.out.printf("The probability that two people share a birthday in a %d person sized class: %f", classSize,
+        System.out.printf("The probability that two people share a birthday in a %d person sized class: %.2f", classSize,
                 tester.runTrials(10000, classSize)*100);
+
+
     }
 }
